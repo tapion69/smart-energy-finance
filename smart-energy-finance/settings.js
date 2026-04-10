6613d052c3d72d509bc238e47764b4c693cf6086
tapion69 <151565403+tapion69@users.noreply.github.com>
2026-04-10 16:45:52 +0200

    Update settings.js

--- smart-energy-finance/settings.js
+++ smart-energy-finance/settings.js
@@ -1,41 +1,45 @@
+const path = require("path");
+
 module.exports = {
     uiPort: process.env.PORT || 1892,
+    uiHost: "0.0.0.0",
 
-    mqttReconnectTime: 15000,
-    serialReconnectTime: 15000,
-
-    debugMaxLength: 1000,
-
-    flowFile: 'flows.json',
+    flowFile: "flows.json",
     flowFilePretty: true,
 
-    userDir: '/data',
-
-    nodesDir: '/opt/node_modules',
+    userDir: "/data",
+    nodesDir: "/opt/node_modules",
 
     credentialSecret: false,
 
-    functionGlobalContext: {
-        fs: require('fs'),
-        path: require('path'),
-        os: require('os'),
-        crypto: require('crypto')
-    },
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+    debugMaxLength: 1000,
 
     contextStorage: {
-        default: {
-            module: 'memory'
-        },
+        default: "memory",
+        memory: { module: "memory" },
         persistent: {
-            module: 'localfilesystem'
+            module: "localfilesystem",
+            config: {
+                dir: path.join("/data", "context"),
+                flushInterval: 30
+            }
         }
+    },
+
+    functionGlobalContext: {
+        fs: require("fs"),
+        path: require("path"),
+        os: require("os"),
+        crypto: require("crypto")
     },
 
     exportGlobalContextKeys: false,
 
     logging: {
         console: {
-            level: 'info',
+            level: "info",
             metrics: false,
             audit: false
         }
